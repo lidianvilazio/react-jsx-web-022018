14ff83bbf5bcd12804a21007a643cdfe09f604fa
lidianvilazio <lidianvilazio@hotmail.com>
2018-04-23 15:54:21 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 // Code goes here
 import React from 'react'
+import ReactDOM from 'react-dom';
 
 import { message } from './components/foo.js';
-import ReactDOM from 'react-dom';
-import fruit from './fruits.js';
+import Fruit from './fruits.js';
 console.log(fruit.apple);
-import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  (
+    <Fruit/>
+  ),
+  document.getElementById('global')
+);
