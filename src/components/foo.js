b93187abccf4bc4ff3b011e88213263ce871b6be
lidianvilazio <lidianvilazio@hotmail.com>
2018-04-23 12:11:18 -0400

    Done.

---
+++ src/components/foo.js
@@ -0,0 +1,4 @@
+export const message = "I am a component!";
+export default message
+// import { apple } from './fruits.js';
+// console.log(apple);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 // Code goes here
+import React from 'react'
+
+import { message } from './components/foo.js';
+import ReactDOM from 'react-dom';
+import fruit from './fruits.js';
+console.log(fruit.apple);
+import ReactDOM from 'react-dom';
